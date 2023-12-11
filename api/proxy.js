544016528c1e4edfc1d55b2b4001f62fd86e02d8
xxcyou <xxcyou@xxcyou.com>
2023-12-12 01:20:50 +0800

    1.3.5

--- api/proxy.js
+++ api/proxy.js
@@ -1,4 +1,4 @@
-//const request = require('request');
+const request = require('request');
 module.exports = (req, res) => {
     let prefix = "/img/tibet-1.jpg"
     let prefix1 = "/favicon.ico"
@@ -10,30 +10,15 @@ module.exports = (req, res) => {
         res.end();
         return;
     }
-
-    main(req)
-        .then(req1 => {
-            res.send(req1);
-            res.end();
-        })
-        .catch(req1 => {
-            res.json({
-                status: 2,
-                msg: req1
-            })
-        })
+    let target = "https://y4cc.cc/l" + req.url;
+    request.get({
+        url:target,
+        headers:{
+            'Content-Type': 'application/octet-stream'
+        }
+    }).on('response',function (response) {
+        console.log(response.statusCode)
+        this.pipe(res)
+    })
 }
 
-const main = (url) => new Promise(async (resolve, reject) => {
-    const origin = url.headers.get("origin") ?? "*";
-    let target = "https://y4cc.cc/l" + url.url;
-    console.log('日志：' + url.url)
-    url = new Request(target, url);
-    url = new Request(url, {redirect: "follow"});
-    let response = await fetch(url);
-    response = new Response(response.body, response);
-    response.headers.delete("set-cookie");
-    response.headers.set("Access-Control-Allow-Origin", origin);
-    response.headers.append("Vary", "Origin");
-    resolve(response)
-})
