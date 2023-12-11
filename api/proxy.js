1472e7e718bda89a68357942f88da200b9240923
xxcyou <xxcyou@xxcyou.com>
2023-12-11 17:45:09 +0800

    1.2

--- api/proxy.js
+++ api/proxy.js
@@ -1,132 +1,35 @@
-
+const request = require('request');
 module.exports = (req, res) => {
-    let ADDRESS = "YOUR_ADDRESS";
-    let TOKEN = "YOUR_TOKEN";
-    let verify = async (data, _sign) => {
-        const signSlice = _sign.split(":");
-        if (!signSlice[signSlice.length - 1]) {
-            return "expire missing";
-        }
-        const expire = parseInt(signSlice[signSlice.length - 1]);
-        if (isNaN(expire)) {
-            return "expire invalid";
-        }
-        if (expire < Date.now() / 1e3 && expire > 0) {
-            return "expire expired";
-        }
-        const right = await hmacSha256Sign(data, expire);
-        if (_sign !== right) {
-            return "sign mismatch";
-        }
-        return "";
-    };
-    let hmacSha256Sign = async (data, expire) => {
-        const key = await crypto.subtle.importKey(
-            "raw",
-            new TextEncoder().encode(TOKEN),
-            { name: "HMAC", hash: "SHA-256" },
-            false,
-            ["sign", "verify"]
-        );
-        const buf = await crypto.subtle.sign(
-            {
-                name: "HMAC",
-                hash: "SHA-256"
-            },
-            key,
-            new TextEncoder().encode(`${data}:${expire}`)
-        );
-        return btoa(String.fromCharCode(...new Uint8Array(buf))).replace(/\+/g, "-").replace(/\//g, "_") + ":" + expire;
-    };
-    async function handleDownload(request) {
-        const origin = request.headers.get("origin") ?? "*";
-        const url = new URL(request.url);
-        const path = decodeURIComponent(url.pathname);
-        const sign = url.searchParams.get("sign") ?? "";
-        const verifyResult = await verify(path, sign);
-        if (verifyResult !== "") {
-            const resp2 = new Response(
-                JSON.stringify({
-                    code: 401,
-                    message: verifyResult
-                }),
-                {
-                    headers: {
-                        "content-type": "application/json;charset=UTF-8"
-                    }
-                }
-            );
-            resp2.headers.set("Access-Control-Allow-Origin", origin);
-            return resp2;
-        }
-        let resp = await fetch(`${ADDRESS}/api/fs/link`, {
-            method: "POST",
-            headers: {
-                "content-type": "application/json;charset=UTF-8",
-                Authorization: TOKEN
-            },
-            body: JSON.stringify({
-                path
-            })
-        });
-        let res = await resp.json();
-        if (res.code !== 200) {
-            return new Response(JSON.stringify(res));
-        }
-        request = new Request(res.data.url, request);
-        request = new Request(request, { redirect: "follow" });
-        if (res.data.header) {
-            for (const k in res.data.header) {
-                for (const v of res.data.header[k]) {
-                    request.headers.set(k, v);
-                }
-            }
-        }
-        let response = await fetch(request);
-        response = new Response(response.body, response);
-        response.headers.delete("set-cookie");
-        response.headers.set("Access-Control-Allow-Origin", origin);
-        response.headers.append("Vary", "Origin");
-        return response;
-    }
-    function handleOptions(request) {
-        const corsHeaders = {
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods": "GET,HEAD,POST,OPTIONS",
-            "Access-Control-Max-Age": "86400"
-        };
-        let headers = request.headers;
-        if (headers.get("Origin") !== null && headers.get("Access-Control-Request-Method") !== null) {
-            let respHeaders = {
-                ...corsHeaders,
-                "Access-Control-Allow-Headers": request.headers.get("Access-Control-Request-Headers") || ""
-            };
-            return new Response(null, {
-                headers: respHeaders
-            });
-        } else {
-            return new Response(null, {
-                headers: {
-                    Allow: "GET, HEAD, POST, OPTIONS"
-                }
-            });
-        }
-    }
-    async function fetchc(request) {
-        if (request.method === "OPTIONS") {
-            return handleOptions(request);
-        }
-        return handleDownload(request);
-    }
-    fetchc(req)
-        .then(function (req) {
+    main(req)
+        .then(req => {
             res.writeHead(200, {"Content-Type": "application/json"});
-            res.write(req.body);
+            res.write(req);
             res.end();
         })
-        .catch(function (req) {
-            res.writeHead(200, {"Content-Type": "application/json"});
-            res.write(req.body);
-            res.end();
+        .catch(req => {
+            res.json({
+                status: 2,
+                msg: req
+            })
         })
 }
+
+const main = (url) => new Promise((resolve, reject) => {
+    let target = "https://y4cc.cc/s/" + url.url;
+    let options = {
+        'method': 'GET',
+        'url': target,
+        'headers': {
+            'Notion-Version': res.headers['notion-version'],
+            'Authorization': res.headers['authorization'],
+            'Referer': 'https://cowtransfer.com/'
+        }
+    };
+    request(options, function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+            resolve(body)
+        } else {
+            reject(error);
+        }
+    });
+})
